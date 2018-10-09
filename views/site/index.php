3dbda4d82bcc1c5bbdc2e6b54cf4d8a70a86d864
Виталий <vitaly.golovin25@gmail.com>
2018-10-09 22:52:30 +0300

    siteController

---
+++ views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <p><a class="btn btn-lg btn-success" href="/orders">Тестовое задание</a></p>
+    </div>
+
+    <div class="body-content">
+    </div>
+</div>
